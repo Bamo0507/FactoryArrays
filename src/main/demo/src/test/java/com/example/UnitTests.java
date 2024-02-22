aacdea7ba694ae768661aee07ce52daa57987958
adrianap0607 <adrianap2670@gmail.com>
2024-02-21 19:51:44 -0600

    JUnit calculadora

--- src/main/demo/src/test/java/com/example/UnitTests.java
+++ src/main/demo/src/test/java/com/example/UnitTests.java
@@ -3,7 +3,63 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.EmptyStackException;
 
-public class PilaTests {
+
+public class CalculadoraTest {
+
+    @Test
+    public void testEsDigito() {
+        assertTrue(Calculadora.esDigito('5'));
+        assertFalse(Calculadora.esDigito('+'));
+    }
+
+    @Test
+    public void testSeparacion() {
+        ArrayList<String> resultado = Calculadora.Separacion("3+5");
+        assertEquals(3, resultado.size());
+        assertEquals("3", resultado.get(0));
+        assertEquals("+", resultado.get(1));
+        assertEquals("5", resultado.get(2));
+    }
+
+    @Test
+    public void testCalcularConStack() {
+        String postfix = "3 5 +";
+        String seleccion = "STACK";
+        String tipoADT = "STACK";
+
+        int resultado = Calculadora.calcular(postfix, seleccion, tipoADT);
+        assertEquals(8, resultado);
+    }
+
+    @Test
+    public void testCalcularConLista() {
+        String postfix = "3 5 +";
+        String seleccion = "LISTA"; // Ajustar según la implementación real
+        String tipoADT = "LISTA";
+
+        int resultado = Calculadora.calcular(postfix, seleccion, tipoADT);
+        assertEquals(8, resultado);
+    }
+
+    @Test
+    public void testCalcularDivisionPorCero() {
+        String postfix = "5 0 /";
+        String seleccion = "STACK";
+        String tipoADT = "STACK";
+
+        int resultado = Calculadora.calcular(postfix, seleccion, tipoADT);
+        assertEquals(0, resultado); // Se espera que se haya reemplazado el 0 por 1
+    }
+
+    @Test
+    public void testCalcularOperadorNoValido() {
+        String postfix = "3 5 $"; // $ no es un operador válido
+        String seleccion = "VECTOR"; // Ajustar según la implementación real
+        String tipoADT = "LISTA";
+
+        int resultado = Calculadora.calcular(postfix, seleccion, tipoADT);
+        assertEquals(0, resultado); // Se espera que el resultado sea 0 debido al operador no válido
+    }
 
     @Test
     public void testArrayListStack() {
